0031f3023d90a4409ae450ef861f6c7af33eb9d8
Fabrice Dugas <fabby.dugas@gmail.com>
2019-09-19 18:07:52 -0400

    Update dataset when text changed and add remove button

--- goal/app/src/main/java/SetsAdapter.kt
+++ goal/app/src/main/java/SetsAdapter.kt
@@ -1,4 +1,6 @@
 import android.support.v7.widget.RecyclerView
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -7,32 +9,57 @@ import com.fabbyd.goal.R
 import kotlinx.android.synthetic.main.item_repetition.view.*
 
 class SetsAdapter(private val dataset: ArrayList<Int>) :
-    RecyclerView.Adapter<SetsAdapter.RepetitionViewHolder>() {
+    RecyclerView.Adapter<SetsAdapter.ViewHolder>() {
 
     // Provide a reference to the views for each data item
     // Complex data items may need more than one view per item, and
     // you provide access to all the views for a data item in a view holder.
     // Each data item is just a string in this case that is shown in a TextView.
-    class RepetitionViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+    class ViewHolder(view: View, val watcher: RepetitionTextWatcher) : RecyclerView.ViewHolder(view) {
         val text : TextView
 
         init {
             text = view.repetition
+            text.addTextChangedListener(watcher)
+        }
+    }
+
+    inner class RepetitionTextWatcher : TextWatcher {
+        var position: Int = 0
+
+        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            // no op
+        }
+
+        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            val str = s.toString()
+            try {
+                val i = str.toInt()
+                dataset[position] = i
+            }
+            catch (ex: NumberFormatException) {
+                // not a number
+            }
+        }
+
+        override fun afterTextChanged(s: Editable?) {
+            // no op
         }
     }
 
     // Create new views (invoked by the layout manager)
     override fun onCreateViewHolder(parent: ViewGroup,
-                                    viewType: Int): RepetitionViewHolder {
+                                    viewType: Int): ViewHolder {
         // create a new view
         val view = LayoutInflater.from(parent.context)
             .inflate(R.layout.item_repetition, parent, false)
 
-        return RepetitionViewHolder(view)
+        return ViewHolder(view, RepetitionTextWatcher())
     }
 
     // Replace the contents of a view (invoked by the layout manager)
-    override fun onBindViewHolder(holder: RepetitionViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.watcher.position = holder.adapterPosition
         holder.text.repetition.setText(dataset[position].toString())
     }
 
--- goal/app/src/main/java/com/fabbyd/goal/activities/FinishExerciseActivity.kt
+++ goal/app/src/main/java/com/fabbyd/goal/activities/FinishExerciseActivity.kt
@@ -1,24 +1,29 @@
 package com.fabbyd.goal.activities
 
 import SetsAdapter
+import android.content.Context
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
+import android.view.View
+import android.widget.EditText
 import com.fabbyd.goal.R
 import kotlinx.android.synthetic.main.activity_finish_exercise.*
 
 class FinishExerciseActivity() : AppCompatActivity() {
     private lateinit var recyclerView: RecyclerView
     private lateinit var viewAdapter: RecyclerView.Adapter<*>
-    private lateinit var viewManager: RecyclerView.LayoutManager
+    private lateinit var viewManager: LinearLayoutManager
     private val sets: ArrayList<Int> = ArrayList()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_finish_exercise)
 
-        viewManager = LinearLayoutManager(this)
+        sets.add(0)
+
+        viewManager = SetsLayoutManager(this)
         viewAdapter = SetsAdapter(sets)
 
         recyclerView = findViewById<RecyclerView>(R.id.sets_recycler_view).apply {
@@ -34,8 +39,29 @@ class FinishExerciseActivity() : AppCompatActivity() {
         }
 
         add_set_button.setOnClickListener {
-            sets.add(sets.count())
+            sets.add(0)
             viewAdapter.notifyDataSetChanged()
+            scrollToLast()
+        }
+
+        remove_set_button.setOnClickListener {
+            if (sets.count() > 0) {
+                sets.removeAt(sets.count()-1)
+                viewAdapter.notifyDataSetChanged()
+            }
+            if (sets.count() > 0)
+                scrollToLast()
+        }
+    }
+
+    private fun scrollToLast() {
+        recyclerView.smoothScrollToPosition(viewAdapter.itemCount-1)
+    }
+
+    private class SetsLayoutManager(context: Context) : LinearLayoutManager(context) {
+        override fun addView(child: View?) {
+            super.addView(child)
+            child?.findViewById<EditText>(R.id.repetition)?.requestFocus()
         }
     }
 }
